0d349845fcd15694887ad3a506bd966ac9159683
wjn18 <wuandjianing18@gmail.com>
2026-05-04 20:28:38 +0100

    Sync UE5 demo links with resume

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import profilePhoto from "../IDphoto.PNG";
 const unityDemoVideoHref =
   "https://www.bilibili.com/video/BV1gvXiBSEjx/?share_source=copy_web&vd_source=a2e61c6d61d8bfc0fad6ebcde9ac1d85";
 const ue5DemoVideoHref =
-  "https://www.bilibili.com/video/BV1nsocB5EY5/?share_source=copy_web&vd_source=a2e61c6d61d8bfc0fad6ebcde9ac1d85";
+  "https://www.bilibili.com/video/BV1hVRBB8En3/?share_source=copy_web&vd_source=a2e61c6d61d8bfc0fad6ebcde9ac1d85";
 const unityDemoDesignHref = "/docs/unity3d-arpg-battle-system-demo-design.pdf";
-const ue5DemoFileHref = "/docs/ue5-open-world-city-level-demo.pdf";
-const ue5DemoDesignHref = "/docs/ue5-open-world-city-level-demo.pdf";
+const ue5DemoFileHref = "https://pan.baidu.com/s/1IPX7dLClaLcBYdBtzkrzgQ?pwd=h5rq";
+const ue5DemoDesignHref = "https://www.kdocs.cn/l/cgvYIQXVailB";
 
 const profile = {
   name: "吴嘉宁",
